c6f961328fef08f68d2138b48bf3ab10f77d2c0a
juan david gaviria guzman <jd58_2g@hotmail.com>
2025-07-04 22:29:00 -0500

    cambio de contraseña de la BD

--- src/conexion/ConexionBD.java
+++ src/conexion/ConexionBD.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class ConexionBD {
     private static final String URL = "jdbc:postgresql://localhost:5432/tienda";
     private static final String USER = "postgres";
-    private static final String PASS = "tu_contraseña";
+    private static final String PASS = "arrieta2025";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
